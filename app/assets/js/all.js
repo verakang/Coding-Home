62b1dd2b136081c8860a525aaa37381d38d6afeb
Vera <sk70885@hotmail.com>
2022-08-26 00:20:09 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,55 @@
+$(function () {
+  var swiper = new Swiper(".mySwiper-type", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    breakpoints: {
+      640: {
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 'auto',
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 'auto',
+        spaceBetween: 50,
+      },
+    },
+  });
+
+  var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
+
+  var swiper = new Swiper(".mySwiper-button", {
+    slidesPerView: 'auto',
+    freeMode: true,
+    spaceBetween: 10,
+    breakpoints: {
+      640: {
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 'auto',
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 'auto',
+        spaceBetween: 50,
+      },
+    },
+  });
+});
