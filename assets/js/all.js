0be7d5c1bafc414a02f58081f8553de6824c7048
Vera <sk70885@hotmail.com>
2022-08-26 00:21:31 +0800

    Update 2022-08-25T16:21:29.183Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,3 +1,5 @@
+"use strict";
+
 $(function () {
   var swiper = new Swiper(".mySwiper-type", {
     slidesPerView: 1,
@@ -5,34 +7,32 @@ $(function () {
     breakpoints: {
       640: {
         slidesPerView: 'auto',
-        spaceBetween: 20,
+        spaceBetween: 20
       },
       768: {
         slidesPerView: 'auto',
-        spaceBetween: 40,
+        spaceBetween: 40
       },
       1024: {
         slidesPerView: 'auto',
-        spaceBetween: 50,
-      },
-    },
+        spaceBetween: 50
+      }
+    }
   });
-
   var swiper = new Swiper(".mySwiper", {
     cssMode: true,
     loop: true,
     navigation: {
       nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      prevEl: ".swiper-button-prev"
     },
     pagination: {
       el: ".swiper-pagination",
-      clickable: true,
+      clickable: true
     },
     mousewheel: true,
-    keyboard: true,
+    keyboard: true
   });
-
   var swiper = new Swiper(".mySwiper-button", {
     slidesPerView: 'auto',
     freeMode: true,
@@ -40,16 +40,17 @@ $(function () {
     breakpoints: {
       640: {
         slidesPerView: 'auto',
-        spaceBetween: 20,
+        spaceBetween: 20
       },
       768: {
         slidesPerView: 'auto',
-        spaceBetween: 40,
+        spaceBetween: 40
       },
       1024: {
         slidesPerView: 'auto',
-        spaceBetween: 50,
-      },
-    },
+        spaceBetween: 50
+      }
+    }
   });
 });
+//# sourceMappingURL=all.js.map
